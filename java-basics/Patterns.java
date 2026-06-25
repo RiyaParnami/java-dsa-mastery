03ca3fa2e76a3045fe0133c241ba0cf4d1df74b6
RiyaParnami <riyaparnami2003@gmail.com>
2026-06-26 00:00:07 +0530

    feat(java-basics): add more pattern implementations

--- java-basics/Patterns.java
+++ java-basics/Patterns.java
@@ -3,6 +3,64 @@ package com.riya;
 public class Patterns {
     public static void main(String[] args) {
         pattern2(4);
+        pattern4(4);
+        pattern5(5);
+        pattern28(5);
+        pattern30(5);
+    }
+
+    static void pattern30(int n){
+        for (int row = 0; row <= n; row++) {
+
+            for (int space = 0; space < n - row; space++) {
+                System.out.print("  ");
+            }
+            for (int col = row; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    static void pattern28(int n){
+        for (int row = 0; row < 2 * n; row++) {
+            int totalColsInRow = row > n ? 2 * n - row : row;
+
+            int noOfSpaces = n - totalColsInRow;
+            for (int s = 0; s < noOfSpaces; s++) {
+                System.out.print(" ");
+            }
+
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern5(int n){
+        for (int row = 0; row < 2 * n; row++) {
+            int totalColsInRow = row > n ? 2 * n - row : row;
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4(int n) {
+        for (int row = 1; row <= n; row++) {
+            // for every row, run the col
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            // when one row is printed, we need to add a newline
+            System.out.println();
+        }
     }
 
     static void pattern3(int n) {
