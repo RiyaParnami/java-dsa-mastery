93e91f444deee28cca816772aba1a277c71ad3fc
RiyaParnami <riyaparnami2003@gmail.com>
2026-06-26 23:33:36 +0530

    feat(java-basics): update patterns with pattern17 and pattern31

--- java-basics/Patterns.java
+++ java-basics/Patterns.java
@@ -3,10 +3,30 @@ package com.riya;
 public class Patterns {
     public static void main(String[] args) {
         pattern2(4);
+        System.out.println();
         pattern4(4);
+        System.out.println();
         pattern5(5);
+        System.out.println();
+        pattern17(5);
+        System.out.println();
         pattern28(5);
+        System.out.println();
         pattern30(5);
+        System.out.println();
+        pattern31(4);
+    }
+
+    static void pattern31(int n) {
+        int originalN = n;
+        n = 2 * n;
+        for (int row = 0; row <= n; row++) {
+            for (int col = 0; col <= n; col++) {
+                int atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n - row, n - col));
+                System.out.print(atEveryIndex + " ");
+            }
+            System.out.println();
+        }
     }
 
     static void pattern30(int n){
@@ -38,6 +58,26 @@ public class Patterns {
             for (int col = 0; col < totalColsInRow; col++) {
                 System.out.print("* ");
             }
+            System.out.println();
+        }
+    }
+
+    static void pattern17(int n) {
+        for (int row = 1; row <= 2 * n; row++) {
+
+            int c = row > n ? 2 * n - row: row;
+
+            for (int space = 0; space < n-c; space++) {
+                System.out.print("  ");
+            }
+
+            for (int col = c; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= c; col++) {
+                System.out.print(col + " ");
+            }
+
             System.out.println();
         }
     }
